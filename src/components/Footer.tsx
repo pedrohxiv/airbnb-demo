940edb235deceb7b99946284afab2861b2d6e3eb
Pedro <pedrohalmeidamendonca@gmail.com>
2023-06-24 18:17:30 -0300

    First day building

---
+++ src/components/Footer.tsx
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <footer className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>About Us</p>
+        <p>Our Team</p>
+        <p>Careers</p>
+        <p>Press</p>
+        <p>Partnerships</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Community Guidelines</p>
+        <p>Events</p>
+        <p>Blog</p>
+        <p>Affiliates</p>
+        <p>Ambassadors</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Become a Host</p>
+        <p>Host Responsibility</p>
+        <p>Host Tools</p>
+        <p>Host Insurance</p>
+        <p>Community Center</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Contact Us</p>
+        <p>FAQs</p>
+        <p>Terms of Service</p>
+        <p>Privacy Policy</p>
+        <p>Report Abuse</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
